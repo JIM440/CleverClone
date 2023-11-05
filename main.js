1556eed9d22e9c884c8640a24d0b32489de15ecf
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-11-05 20:02:49 +0100

    final changes

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ menu.addEventListener('click', ()=>{
 mobileMenu.classList.toggle('active')
 menu.classList.toggle('active')
 })
+
+// Select the element to be revealed
+const h1 = document.querySelector('h1');
+
+// Create the intersection observer
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('reveal');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { once: true });
+
+// Start observing the element
+observer.observe(h1);
+
+
+const revealElements = document.querySelectorAll('.scroll-reveal');
+
+// Start observing each element
+revealElements.forEach((element) => {
+  observer.observe(element);
+});
